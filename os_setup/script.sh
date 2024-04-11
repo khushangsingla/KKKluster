9b230f3cdf23a831dcfdbaea0730b7edbf5f47f1
Hrishikesh <hrishijedhe06@gmail.com>
2024-04-11 19:10:25 +0530

    Updated to remove a few errors

--- os_setup/script.sh
+++ os_setup/script.sh
@@ -3,37 +3,52 @@ end_uefi=300MiB
 start_swap=$end_uefi
 end_swap=1GiB
 start_root=$end_swap
-device_name=sda
+device_name=nbd5
 bootstrap_mirror=http://deb.debian.org/debian/
-parted -s \ # non-interactive
-	-a optimal \ # align according to optimal performance
-	-- /dev/$device_name \ # block device to set the partitions
-	mklabel gpt \ # make a gpt partition table
-	mkpart primary uefi fat32 $start_uefi $end_uefi \ # make a UEFI partition
-	mkpart primary swap linux-swap $start_swap $end_swap \ # make a swap partition
-	mkpart primary root ext4 $start_root -0 \ # make root partition
-	set 1 esp on \ # set esp flag on efi partition
-	set 2 swap on \ # set swap flag on
-	set 3 root on \ # set root flag on
-	set 1 boot on # set boot flag on
+# grub-install --target=i386-pc /dev/${device_name}
+parted -s  `# non-interactive` \
+	-a optimal `# align according to optimal performance` \
+	-- /dev/$device_name `# block device to set the partitions` \
+	mklabel gpt `# make a gpt partition table` \
+	mkpart primary fat32 $start_uefi $end_uefi `# make a UEFI partition` \
+	mkpart primary linux-swap $start_swap $end_swap `# make a swap partition` \
+	mkpart primary ext4 $start_root -0 `# make root partition` \
+	name 1 uefi \
+	name 2 swap \
+	name 3 root \
+	set 1 esp on `# set esp flag on efi partition` \
+	set 2 swap on `# set swap flag on`
+	# set 1 boot on # set boot flag on
 
 
 # formatting the partitions
 
-mkfs -t fat -F 32 -n EFI /dev/${device_name}p1
+mkfs.fat -F 32 /dev/${device_name}p1
 mkswap -L swap /dev/${device_name}p2
-mkfs -t ext4 -L root /dev/${device_name}p3
+mkfs.ext4 -L root /dev/${device_name}p3
 
-swap_uuid="$(blkid | grep '^/dev/${device_name}p2' | grep -o ' UUID="[^"]\+"' | sed -e 's/^ //' )"
-root_uuid="$(blkid | grep '^/dev/${device_name}p3' | grep -o ' UUID="[^"]\+"' | sed -e 's/^ //' )"
-boot_uuid="$(blkid | grep '^/dev/${device_name}p1' | grep -o ' UUID="[^"]\+"' | sed -e 's/^ //' )"
+# swap_uuid="$(blkid --probe /dev/${device_name}p2 | grep -o ' UUID="[^"]\+"' | sed -e 's/^ //' )"
+# root_uuid="$(blkid --probe /dev/${device_name}p3 | grep -o ' UUID="[^"]\+"' | sed -e 's/^ //' )"
+# boot_uuid="$(blkid --probe /dev/${device_name}p1 | grep -o ' UUID="[^"]\+"' | sed -e 's/^ //' )"
 
 mount /dev/${device_name}p3 /mnt
+
+mkdir /mnt/boot
 mount /dev/${device_name}p1 /mnt/boot
 
-debootstrap --arch=amd64 \ # architecture of machine
-	--include=docker.io,docker-compose,openssh-server,network-manager \ # packages to install
-	--components= \ # components to use
-	bookworm \ # install stable
-	/mnt \ #install target
+swapon /dev/${device_name}p2
+
+debootstrap --arch=amd64 `# architecture of machine` \
+	--include=docker.io,docker-compose,openssh-server,grub-efi-amd64,linux-image-amd64,network-manager,dbus,dbus-bin,dbus-daemon,dbus-session-bus-common,dbus-system-bus-common,dbus-user-session,libpam-systemd,systemd `# packages to install` \
+	--components=main,restricted,universe `# components to use` \
+	bookworm `# install stable` \
+	/mnt `#install target` \
 	$bootstrap_mirror # mirror for bootstrapping
+
+genfstab -U /mnt >> /mnt/etc/fstab
+# mount --bind /dev /mnt/dev
+# mount --bind /proc /mnt/proc
+# mount --bind /sys /mnt/sys
+
+arch-chroot /mnt/ bash -c "apt update && apt upgrade -y && apt install grub-efi-amd64 linux-image-amd64 sudo && grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB && update-grub && grub-mkconfig -o /boot/grub/grub.cfg && useradd --home /home/admin --shell /bin/bash -m admin && echo the_password | passwd admin --stdin && echo 'admin  ALL=(ALL:ALL) ALL' >> /etc/sudoers"
+umount -R /mnt
